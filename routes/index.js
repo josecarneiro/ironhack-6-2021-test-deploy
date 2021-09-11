f643fa68dc6caec502ec6b1f53dc799085ed2f6d
oddouu <oddouu@gmail.com>
2021-09-11 11:40:36 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const routeGuard = require('./../middleware/route-guard');
+const test = "";
 
 router.get('/', (req, res, next) => {
   res.render('home', { title: 'Hello World!' });
